6240a47ca32e6129c3278a08397d44c3571a4644
Ravi <ravikumar.tamanampudi@realogy.com>
2021-02-19 14:53:49 -0500

    commit

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -39,7 +39,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int Id)
         {
-            var item = _context.Items.FirstOrDefault(i => i.Id == Id);
+            Item item = _context.Items.FirstOrDefault(i => i.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
