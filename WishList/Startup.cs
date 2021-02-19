9d72eefb2d625bd118c93da90dd206f52939046c
Ravi <ravikumar.tamanampudi@realogy.com>
2021-02-19 13:02:05 -0500

    commit

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -33,6 +33,7 @@ namespace WishList
             }
             
             app.UseRouting();
+
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
