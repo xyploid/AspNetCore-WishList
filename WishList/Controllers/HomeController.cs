aa798d9f75306de101ac947947d125379d38497e
Ravi <ravikumar.tamanampudi@realogy.com>
2021-02-19 13:14:38 -0500

    commit

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Error()
